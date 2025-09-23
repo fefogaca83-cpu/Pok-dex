0c74575ac1de995048c20a3d6a47672a9a9052cf
Felipe Fogaça <fe.fogaca83@gmail.com>
2025-09-22 22:44:47 -0300

    teste

--- src/app/services/PokemonService.ts
+++ src/app/services/PokemonService.ts
@@ -12,7 +12,7 @@ export class PokemonService {
   async getByPege(page: number, size: number = 40): Promise<Resultado[]> {
     if (page > 5) return [];
     const offset = size * (page - 1);
-    const res = await fetch(`/api/pokemon/?limit=${size}&offset=${offset}`);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${size}&offset=${offset}`);
     const resJason = await res.json();
     if (resJason.results.length > 0) return resJason.results;
     return [];
@@ -20,14 +20,14 @@ export class PokemonService {
 
   async getById(id: string): Promise<Pokemon> {
     console.log('🔍 Buscando pokemon ID:', id);
-    const res = await fetch(`/api/pokemon/${id}`);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const pokemon = await res.json();
     console.log('✅ Pokemon encontrado:', pokemon.name, 'Altura:', pokemon.height, 'Peso:', pokemon.weight);
     return pokemon;
   }
 
   async getDescricao(id: string | number): Promise<string> {
-    const res = await fetch(`/api/pokemon-species/${id}`);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
     const resJson = await res.json();
     const texto = resJson.flavor_text_entries.find((texto: any) => texto.language.name === 'es');
     return texto.flavor_text;
